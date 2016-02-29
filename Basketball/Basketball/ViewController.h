4db442928a1761f15a4e7df39dd5edf5824978d4
Allen <1214362919@qq.com>
2016-02-29 09:54:51 +0800

    修改.gitignore文件，添加一些常用pod

---
+++ Basketball/Basketball/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Basketball
+//
+//  Created by Allen on 2/29/16.
+//  Copyright © 2016 wgl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Basketball/Basketball/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Basketball
+//
+//  Created by Allen on 2/29/16.
+//  Copyright © 2016 wgl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
